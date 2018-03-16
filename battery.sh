61646e76bdc084d20223e44ed6e94d3ad9317e8b
boss <gman@hl.com>
2018-03-16 15:12:14 +0500

    prev version didnt work

--- battery.sh
+++ battery.sh
@@ -2,7 +2,7 @@
 BAT_CAP=/sys/class/power_supply/BAT0/capacity
 while true
 do
-	if [ $(< $BAT_CAP) -lt 10 ]
+	if [ $( cat $BAT_CAP ) -lt 10 ]
 	then
 		notify-send -u critical -t 20000 "battery" "low level"
 	fi
