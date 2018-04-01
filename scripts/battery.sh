abda70ce94cce59208e8357481dc3646a0db24df
boss <gman@hl.com>
2018-04-01 11:27:33 +0500

    hibernate when lower than 5% and ac is not connected

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,10 +1,36 @@
-#!/bin/bash
-BAT_CAP=/sys/class/power_supply/BAT0/capacity
+#!/bin/sh
+LVL_LOW=10
+LVL_CRIT=5
+BAT_PATH='/sys/class/power_supply/BAT0'
+AC_PATH='/sys/class/power_supply/AC0'
+CRIT_TIMEOUT=false
 while true
 do
-	if [ $( cat $BAT_CAP ) -lt 10 ]
-	then
-		notify-send -u critical -t 20000 "battery" "low level"
+	BAT_CAP=$( < $BAT_PATH/capacity )
+	AC_STATE=$( < $AC_PATH/online )
+	if [ $AC_STATE -eq 1 ]; then
+		if $CRIT_TIMEOUT
+		then
+			CRIT_TIMEOUT=false
+		fi
+		sleep 5m
+	else
+		if $CRIT_TIMEOUT 
+		then
+			CRIT_TIMEOUT=false
+			$HOME/scripts/lock.sh && systemctl hibernate
+		fi
+		if [ $BAT_CAP -lt $LVL_LOW ]; then
+			if [ $BAT_CAP -gt $LVL_CRIT ]; then
+				notify-send -u critical -t 20000 "battery" "low level"
+				sleep 2m
+			else
+				notify-send -u critical -t 20000 "battery" "hibernation after 1 min"
+				CRIT_TIMEOUT=true
+				sleep 1m
+			fi
+		else
+			sleep 5m
+		fi
 	fi
-	sleep 5m
 done
